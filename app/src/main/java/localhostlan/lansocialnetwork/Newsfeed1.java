919deaf70a719cab9c2a7638240a673653b682cb
Brittany Morris <bmorris03@aurora.edu>
2016-04-22 11:28:54 -0500

    Fixed java files & remade GUI

---
+++ app/src/main/java/localhostlan/lansocialnetwork/Newsfeed1.java
@@ -0,0 +1,13 @@
+package localhostlan.lansocialnetwork;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Newsfeed1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newsfeed1);
+    }
+}
